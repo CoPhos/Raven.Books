4871cfdd902a2ac7cccfe9cfc24b1960e19ed8c5
jorge-cobos <cobos1932@gmail.com>
2021-08-23 16:36:17 -0500

    Defined books models

--- root/backend/books/models.py
+++ root/backend/books/models.py
@@ -1,3 +1,44 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Publisher(models.Model):
+    publisher_name = models.CharField(max_length=50, null=False, blank=False)
+
+    def __str__(self):
+        return self.publisher_name
+
+class Authors(models.Model):
+    first_name = models.CharField(max_length=50, null=False, blank=False)
+    last_name = models.CharField(max_length=50, null=False, blank=False)
+    profile_photo = models.ImageField(null=True)
+    about_author = models.TextField(null=False, blank=False)
+
+    def __str__(self):
+        return self.first_name
+
+class Tags(models.Model):
+    tag_name = models.CharField(max_length=50, null=False, blank=False)
+
+    def __str__(self):
+        return self.tag_name
+
+class Book(models.Model):
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, null=False, blank=False)
+    isbn = models.CharField(max_length=17, null=False, blank=False)
+    publication_year = models.DateTimeField()
+    edition = models.CharField(max_length=2, null=False, blank=False)
+    price = models.DecimalField(max_digits=4, decimal_places=2,null=False, blank=False)
+    quant_in_stock = models.BigIntegerField()
+    language = models.CharField(max_length=2, null=False, blank=False)
+    weight = models.CharField(max_length=4, null=False, blank=False)
+    pages = models.CharField(max_length=4, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    tags = models.ManyToManyField('Tags')
+    authors = models.ManyToManyField('Authors')
+
+    def __str__(self):
+        return self.title
+
+
--- root/backend/users/models.py
+++ root/backend/users/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
 # Create your models here.
+
 class CustomUser(AbstractUser):
     first_name = models.CharField(max_length=50, null=False, blank=False)
     last_name = models.CharField(max_length=50, null=False, blank=False)
