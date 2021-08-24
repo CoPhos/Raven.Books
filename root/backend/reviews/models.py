4e386d7774c8aee291272ca1dc171a908fca4658
jorge-cobos <cobos1932@gmail.com>
2021-08-24 12:33:45 -0500

    Defined review models

--- root/backend/reviews/models.py
+++ root/backend/reviews/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Review(models.Model):
+    tittle = models.CharField(max_length=255, null=False, blank=False)
+    date_created = models.DateTimeField(default=timezone.now())
+    text = models.TextField(null=False, blank=False)
+    customer = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE)
+    book = models.ForeignKey('books.Book', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.tittle
+
+class Score(models.Model):
+    score = models.DecimalField(max_digits=2, decimal_places=1, null=False, blank=False)
+    customer = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE)
+    book = models.ForeignKey('books.Book', on_delete=models.CASCADE)
